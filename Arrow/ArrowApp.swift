ec7c236c71ccbb2cf0a3d76e20b610c0d04f8201
Kaan Caliskan <kcaliskan@lcs.on.ca>
2024-11-26 14:26:20 -0500

    Initial Commit

---
+++ Arrow/ArrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArrowApp.swift
+//  Arrow
+//
+//  Created by Ahmet Kaan Caliskan on 2024-11-26.
+//
+
+import SwiftUI
+
+@main
+struct ArrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Arrow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Arrow
+//
+//  Created by Ahmet Kaan Caliskan on 2024-11-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
